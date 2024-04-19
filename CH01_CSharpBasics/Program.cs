7fa6410a34f86d07e4a07a750d8781e7387fb6e7
Ziya <sziya356@gmail.com>
2024-04-19 20:32:08 +0530

    update

---
+++ CH01_CSharpBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Hello world");
+//    }
+//}
+
+using CH01_CSharpBasics;
+
+T01_DataTypes t = new T01_DataTypes();
+t.Test();
