5dbc80f43b11c7c493cedda645b9aa44ad815678
Ziya <sziya356@gmail.com>
2024-05-11 20:30:48 +0530

    update

--- CH02_OOPS/T09_Collections.cs
+++ CH02_OOPS/T09_Collections.cs
@@ -8,7 +8,7 @@
 /// 2. List
 /// 3. Queue
 /// 4. Stack
-/// 5. Tuple
+/// 5. Tuple - Used to return multiple objects
 /// 6. ICollection
 /// 7. IList
 /// 8. IEnumerable
@@ -18,7 +18,8 @@ public class T09_Collections
 {
     public void Test()
     {
-        TestList();
+        var result = TestTuple(new Student() { Id = 1, Name = "AA", Age = 11 });
+        var result1 = ReturnSingleObject(new Student() { Id = 1, Name = "AA", Age = 11 });
     }
 
     public void TestList()
@@ -51,6 +52,57 @@ public class T09_Collections
 
         List<Student> students2 = students1;//Student2 will refer same list
         students2.Add(s1);
+    }
+
+    public void TestQueue()
+    {
+        Student s1 = new Student() { Age = 1, Id = 1, Name = "AAA" };
+        Student s2 = new Student() { Age = 4, Id = 2, Name = "BBB" };
+        Student s3 = new Student() { Age = 5, Id = 3, Name = "CCC" };
+
+        Queue<Student> queue = new Queue<Student>();
+        queue.Enqueue(s1);
+        queue.Enqueue(s2);
+        queue.Enqueue(s3);
+
+        var s11 = queue.Dequeue();
+        var s12 = queue.Dequeue();
+        var s13 = queue.Dequeue();
+        bool isSuccess = queue.TryDequeue(out Student? s14);
+        queue.Enqueue(s1);
+    }
+
+    public void TestStack()
+    {
+        Student s1 = new Student() { Age = 1, Id = 1, Name = "AAA" };
+        Student s2 = new Student() { Age = 4, Id = 2, Name = "BBB" };
+        Student s3 = new Student() { Age = 5, Id = 3, Name = "CCC" };
+        Stack<Student> stack = new Stack<Student>();
+        stack.Push(s1);
+        stack.Push(s2);
+        stack.Push(s3);
+
+        var s21 = stack.Pop();
+        var s22 = stack.Pop();
+        var s23 = stack.Pop();
+        bool isSuccess = stack.TryPop(out Student? s24);
+    }
+
+    public (int Id, string Name, int Age) TestTuple(Student student)
+    {
+        return (student.Id, student.Name, student.Age);
+        //return (1, "Hello", 12);
+    }
+
+    public Student ReturnSingleObject(Student student)
+    {
+        //return new Student()
+        //{
+        //    Name = student.Name,
+        //    Id = student.Id,
+        //    Age = student.Age
+        //};
+        return student;
     }
 }
 
