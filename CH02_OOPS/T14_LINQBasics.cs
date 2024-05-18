56956a5216af07ac7f33bb0e733bfdef774e2156
Ziya <sziya356@gmail.com>
2024-05-18 20:16:08 +0530

    update

--- CH02_OOPS/T14_LINQBasics.cs
+++ CH02_OOPS/T14_LINQBasics.cs
@@ -1,5 +1,22 @@
 ﻿namespace CH02_OOPS;
 
+/// <summary>
+/// LINQ is a query language that helps in performing various operations like
+/// -Where for filtering
+/// -Select for selecting one or more properties/columns of dataset
+/// -Count
+/// -Min
+/// -Max
+/// -Take
+/// -Skip
+/// -First
+/// -FirstOrDefault
+/// -Last
+/// -LastOrDefault
+/// -Single
+/// -SingleOrDefalut
+/// -Distinct
+/// </summary>
 public class T14_LINQBasics
 {
     private List<Student> students;
@@ -21,11 +38,58 @@ public class T14_LINQBasics
         TestWhere();
     }
 
+    public void TestSelect()
+    {
+        //Selecting single column/property of data set
+        var studentIdList = students.Select(SelectorMethod).ToList();
+        studentIdList = students.Select(s => s.Id).ToList();
+        var studentNameList = students.Select(s => s.Name).ToList();
+
+        //Selecting multiple columns of data set
+        var studentList = students.Select(s => new { s.Id, s.Name }).ToList();
+
+        //Selecting multiple columns using another class
+        var subStudentList = students
+            .Select(s => new SubStudent() { Id = s.Id.ToString(), StudentName = s.Name })
+            .ToList();
+        subStudentList = students
+            .Select(s => new SubStudent(s.Id.ToString(), s.Name))
+            .ToList();
+    }
+
+    public class SubStudent
+    {
+#pragma warning disable
+
+        public SubStudent()
+        {
+        }
+
+#pragma warning restore
+
+        public SubStudent(string id, string name)
+        {
+            this.Id = id;
+            this.StudentName = name;
+        }
+
+        public string Id { get; set; }
+        public string StudentName { get; set; }
+    }
+
+    public int SelectorMethod(Student student)
+    {
+        return student.Id;
+    }
+
     public void TestWhere()
     {
+        //Where is a filtering method and requires a function
         //Filtering from a list
         var studentList = students.Where(FilteringFunction).ToList();
         studentList = students.Where(s => s.Age > 12).ToList();
+        studentList = students.Where(s => s.Name.Contains('o')).ToList();
+        studentList = students.Where(s => s.Name.Length == 6 && s.Name.Contains('o')).ToList();
         //Called Function is Where Method
     }
 
