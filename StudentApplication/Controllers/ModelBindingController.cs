3ad641949a60d3491dbab4861e71c62f46f1f4eb
Ziya <sziya356@gmail.com>
2024-06-12 20:28:40 +0530

    update

---
+++ StudentApplication/Controllers/ModelBindingController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApplication.ViewModels;
+
+namespace StudentApplication.Controllers;
+
+public class ModelBindingController : Controller
+{
+    private List<StudentViewModel> students;
+    public ModelBindingController()
+    {
+        students = new List<StudentViewModel>();
+        for (int i = 1; i <= 100; i++) // Loop 50 times (from 5 to 54)
+        {
+            students.Add(new StudentViewModel { Id = i, Name = "Student " + i, Age = 11 + (i - 5), Gender = (i % 2 == 0) ? "M" : "F" });
+        }
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Student()
+    {
+        StudentViewModel model = students[0];
+        return View(model);
+    }
+    public IActionResult Student1()//Scaffolded
+    {
+        StudentViewModel model = students[0];
+        return View(model);
+    }
+    public IActionResult Students()
+    {
+        return View(students);
+    }
+    public IActionResult Students1()//Scaffolded
+    {
+        return View(students);
+    }
+}
---
+++ StudentApplication/ViewModels/StudentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentApplication.ViewModels;
+
+public class StudentViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+}
